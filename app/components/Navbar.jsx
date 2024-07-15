145b9c51a77bea1f6f75d34bf3960de7a987848a
RichardFLW <richard.windels@icloud.com>
2024-07-15 09:30:16 +0200

    initial commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar(){
+
+
+    return(
+
+        <>
+        <div className="bg-blue-500 h-24 ">
+        <h1 className="ml-25 text-4xl">4i1m</h1>
+        </div>
+        </>
+    )
+}
