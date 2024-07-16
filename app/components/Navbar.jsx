bf0194218e533cb1fdf897722b2178318017463b
RichardFLW <richard.windels@icloud.com>
2024-07-16 08:50:03 +0200

    navbar v2

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,14 +1,66 @@
+// app/components/Navbar.js
+
+"use client"
+import React, { useState } from "react";
+import { FaHome, FaGamepad, FaChartBar, FaBars, FaTimes } from "react-icons/fa";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styles from "./Navbar.module.css";
 
-export default function Navbar() {
-    return (
-      <div className="navbar">
+const Navbar = () => {
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
 
-       <p>4i1m</p>
-       <Link href="/"> Acceui</Link>
-       <Link href="/pagedejeu"> Jouer</Link>
-       <Link href="/stats"> stats</Link>
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbarContent}>
+        <div className="text-lg font-bold">4 Images 1 Mot</div>
+        <button onClick={toggleMenu} className={styles.menuButton}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </button>
+        <ul
+          className={`${styles.menuItems} ${
+            isOpen ? styles.menuItemsOpen : ""
+          }`}
+        >
+          <li>
+            <Link
+              href="/"
+              className={`${styles.menuItem} ${
+                pathname === "/" ? styles.menuItemActive : ""
+              }`}
+            >
+              <FaHome className="mr-2" /> Accueil
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/jeu"
+              className={`${styles.menuItem} ${
+                pathname === "/jeu" ? styles.menuItemActive : ""
+              }`}
+            >
+              <FaGamepad className="mr-2" /> Le Jeu
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/stats"
+              className={`${styles.menuItem} ${
+                pathname === "/stats" ? styles.menuItemActive : ""
+              }`}
+            >
+              <FaChartBar className="mr-2" /> Stats
+            </Link>
+          </li>
+        </ul>
       </div>
-    );
-  }
-  
+    </nav>
+  );
+};
+
+export default Navbar;
