901ccebf6353cd992c6d73ca51732c1a3e84b6ce
RichardFLW <richard.windels@icloud.com>
2024-07-18 14:06:06 +0200

    first build ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.pexels.com', // Assurez-vous que c'est le bon hostname
+          port: '',
+          pathname: '/photos/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
